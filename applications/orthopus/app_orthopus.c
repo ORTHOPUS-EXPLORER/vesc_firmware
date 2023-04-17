913063b16436080775e4f9aea6c11d5d8530df0e
BMi <bmi.dev@redox.ws>
2023-04-18 00:26:16 +0200

    Cleanup, split in multiple files, add config (in HW CONFIG), explore LISP stuff, add encoder offset

--- applications/orthopus/app_orthopus.c
+++ applications/orthopus/app_orthopus.c
@@ -40,91 +40,29 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "app_orthopus.h"
+
 // Threads
-static THD_FUNCTION(my_thread, arg);
-static THD_WORKING_AREA(my_thread_wa, 1024);
-static volatile bool stop_now = true;
-static volatile bool is_running = false;
+static THD_WORKING_AREA(orthopus_thread_wa, 1024);
 
-// Private functions
-static void my_pwm_callback(void);
-
-// Private variables
-
-float orthopus_init_offset(float v, bool use_v)
+// Just make sure to pad to a 32bit aligned size.
+// Eg: if you add an uint8_t param, add 3 bytes of padding after.
+//                   uint16_t param     2
+// uint8_t myparam; uint8_t[3] pad;
+static orthopus_config_t orthopus_config =
 {
-  if(!use_v)
-  {
-    v = 0.0; // Reset
-    size_t i = 0;
-    for(i=0;i<3;i++)
-    {
-      chThdSleepMilliseconds(1);
-      v += enc_as504x_read_angle(&encoder_cfg_as504x)/3;
-    }
-  }
+  .encoder_offset = 0.0,
+};
 
-  mc_interface_update_pid_pos_offset(v, false);
-  return v;
-}
 
-static void orthopus_init_offset_cmd(int argc, const char **argv)
-{
-  if(argc > 2)
-  {
-    commands_printf("Invalid arguments. Usage: o_init_offset [v]");
-    return;
-  }
-  float v = 0;
-	if (argc == 2)
-		sscanf(argv[1], "%f", &v);
-  v = orthopus_init_offset(v, argc == 2);
-  commands_printf("Init Pos PID Offset with joint offset: % 3.3f", (double)v);
-}
-
-static lbm_value orthopus_lisp_read_encoder(lbm_value *args, lbm_uint argn)
-{
-	(void)args; (void)argn;
-  enc_as504x_read_angle(&encoder_cfg_as504x);
-	return lbm_enc_float(enc_as504x_read_angle(&encoder_cfg_as504x));
-}
-
-static lbm_value orthopus_lisp_init_offset(lbm_value *args, lbm_uint argn)
-{
-  //LBM_CHECK_ARGN_NUMBER(1);
-  if(argn > 1)
-  {
-    lbm_set_error_reason("Invalid arguments. Usage: ortho-init-offset [v]");
-    return ENC_SYM_EERROR;
-  }
-
-  float v = 0;
-  if(argn == 1)
-    v = lbm_dec_as_float(args[0]);
-  v = orthopus_init_offset(v, argn == 1);
-  commands_printf_lisp("Init Pos PID Offset with joint offset: % 3.3f", (double)v);
-
-  return ENC_SYM_TRUE;
-}
-
-static lbm_uint lisp_v;
-
-void orthopus_init_lisp(void)
-{
-  // Still not clear how this one works...
-  lbm_add_symbol_const("orthopus-val", &lisp_v);
-
-  // in REPL, test with: (print (orthopus-read-encoder))
-  lbm_add_extension("orthopus-read-encoder", orthopus_lisp_read_encoder);
-
-  // in REPL, test with: (orthopus-init-offset) or (orthopus-init-offset 45)
-  lbm_add_extension("orthopus-init-offset", orthopus_lisp_init_offset);
-}
+static void orthopus_process_custom_app_data(unsigned char *rx_d, unsigned int len);
+static void orthopus_process_custom_hw_data(unsigned char *rx_d, unsigned int len);
 
 // Called when the custom application is started. Start our
 // threads here and set up callbacks.
-void app_custom_start(void) {
-  commands_printf("AppStart()");
+void app_custom_start(void)
+{
+  commands_printf("OrthopusAppStart");
 
   SENSOR_PORT_3V3();
   if(!enc_as504x_init(&encoder_cfg_as504x))
@@ -138,95 +76,58 @@ void app_custom_start(void) {
 	palSetPadMode(GPIOB, 10, PAL_MODE_INPUT);
 
   // Hard-RT context
-	mc_interface_set_pwm_callback(my_pwm_callback);
+	mc_interface_set_pwm_callback(orthopus_pwm_callback);
 
-	stop_now = false;
-	chThdCreateStatic(my_thread_wa, sizeof(my_thread_wa),
-			NORMALPRIO, my_thread, NULL);
+	orthopus_thread_stop = false;
+	chThdCreateStatic(orthopus_thread_wa, sizeof(orthopus_thread_wa),
+			NORMALPRIO, orthopus_thread, NULL);
 
   // Add shell commands
-  terminal_register_command_callback(
-    "o_init_offset",
-    "[Orthopus] Initialize Pos PID offset with current AMS (or forced) value",
-    "[d]",
-    orthopus_init_offset_cmd
-  );
+  orthopus_cmd_init();
 
   // Add LISP commands/symbols
   lispif_set_ext_load_callback(&orthopus_init_lisp);
+
+  commands_set_app_data_handler(orthopus_process_custom_app_data);
+  commands_set_hw_data_handler(orthopus_process_custom_hw_data);
 }
 
 // Called when the custom application is stopped. Stop our threads
 // and release callbacks.
-void app_custom_stop(void) {
+void app_custom_stop(void)
+{
 	mc_interface_set_pwm_callback(0);
-	terminal_unregister_callback(orthopus_init_offset_cmd);
+	orthopus_cmd_deinit();
 
-	stop_now = true;
-	while (is_running) {
+	orthopus_thread_stop = true;
+	while (orthopus_thread_running)
 		chThdSleepMilliseconds(1);
-	}
 }
 
 void app_custom_configure(app_configuration *conf) {
 	(void)conf;
+
+  if(!orthopus_config_load(&orthopus_config))
+     commands_printf("Orthopus_config_load failed");
 }
 
-static THD_FUNCTION(my_thread, arg) {
-	(void)arg;
+static void orthopus_process_custom_app_data(unsigned char *rx_d, unsigned int len)
+{
+  (void)rx_d; (void)len;
 
-	chRegSetThreadName("AppCustomTh");
-
-	is_running = true;
-
-  size_t encoder_wait=10;
-  do  {
-    enc_as504x_read_angle(&encoder_cfg_as504x);
-    chThdSleepMilliseconds(100);
-    if(encoder_wait && !(--encoder_wait))
-      break;
-  } while(!encoder_cfg_as504x.state.sensor_diag.is_connected);
-
-  orthopus_init_offset_cmd(0, NULL);
-
-	// Example of using the experiment plot
-//	chThdSleepMilliseconds(8000);
-//	commands_init_plot("Sample", "Voltage");
-//	commands_plot_add_graph("Temp Fet");
-//	commands_plot_add_graph("Input Voltage");
-//	float samp = 0.0;
-//
-//	for(;;) {
-//		commands_plot_set_graph(0);
-//		commands_send_plot_points(samp, mc_interface_temp_fet_filtered());
-//		commands_plot_set_graph(1);
-//		commands_send_plot_points(samp, GET_INPUT_VOLTAGE());
-//		samp++;
-//		chThdSleepMilliseconds(10);
-//	}
-
-	for(;;) {
-		// Check if it is time to stop.
-		if (stop_now) {
-			is_running = false;
-			return;
-		}
-
-		timeout_reset(); // Reset timeout if everything is OK.
-
-		// Run your logic here. A lot of functionality is available in mc_interface.h.
-
-    // You can call  functions such as:
-    // - mc_interface_set_duty()
-    // - mc_interface_set_pid_speed()
-    // - mc_interface_set_pid_pos()
-
-		chThdSleepMilliseconds(10);
-	}
+  unsigned int olen=0;
+  unsigned char tx_d[256];
+  if(olen)
+    commands_send_app_data(tx_d, olen);
 }
 
-// Called in mc_interface.c:1913, in mc_interface_mc_timer_isr()
-static void my_pwm_callback(void) {
-	// Called for every control iteration in interrupt context.
+// FIXME: Figure out how to send these packets from the APP or from CAN
+static void orthopus_process_custom_hw_data(unsigned char *rx_d, unsigned int len)
+{
+  (void)rx_d; (void)len;
 
+  unsigned int olen=0;
+  unsigned char tx_d[256];
+  if(olen)
+    commands_send_hw_data(tx_d, olen);
 }
--- applications/orthopus/app_orthopus_conf.h
+++ applications/orthopus/app_orthopus_conf.h
@@ -2,10 +2,10 @@
 #define APP_ORTHOPUS_CONF_H
 
 #define APP_CUSTOM_TO_USE					"orthopus/app_orthopus.c"
-//#define APP_CUSTOM_TO_USE_2					"orthopus/app_orthopus2.c"
-//#define APP_CUSTOM_TO_USE_3					"orthopus/app_orthopus3.c"
-//#define APP_CUSTOM_TO_USE_4					"orthopus/app_orthopus4.c"
-//#define APP_CUSTOM_TO_USE_5					"orthopus/app_orthopus5.c"
+#define APP_CUSTOM_TO_USE_FILE_2  "orthopus/app_orthopus_cmd.c"
+#define APP_CUSTOM_TO_USE_FILE_3  "orthopus/app_orthopus_lisp.c"
+#define APP_CUSTOM_TO_USE_FILE_4  "orthopus/app_orthopus_utils.c"
+#define APP_CUSTOM_TO_USE_FILE_5  "orthopus/app_orthopus_algo.c"
 
 #define APPCONF_APP_TO_USE					APP_CUSTOM
 
