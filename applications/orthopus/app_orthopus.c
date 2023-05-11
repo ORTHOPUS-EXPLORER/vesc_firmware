8c59e2a51f30c736849ef06f545212ddaaf1feac
Thomas Solatges <thomas.solatges@orthopus.com>
2023-05-11 10:05:18 +0200

    merged config load/Save patch

--- applications/orthopus/app_orthopus.c
+++ applications/orthopus/app_orthopus.c
@@ -70,24 +70,7 @@ void app_custom_start(void)
   // if orthopus config not set, set default values
   if (!orthopus_config.orthopus_config_set)
   {
-<<<<<<< HEAD
-    orthopus_config.encoder_offset                  = 49.7; //for OR14B005 todo set to zero
-    orthopus_config.encoder_filter_anglestep        = 0.25;
-    orthopus_config.encoder_filter_enable           = true;
-    orthopus_config.encoder_filter_plot_enable      = false;
-    orthopus_config.limits_enable                   = false;
-    orthopus_config.orthopus_config_set             = false;
-    orthopus_config.limits_pos_max                  = 90.0;
-    orthopus_config.limits_pos_min                  = -90.0;
-    orthopus_config.angle_division                  = 700;
-    orthopus_config.limits_reach_angle              = 15;
-    orthopus_config.limits_reach_speed              = 2;
-    orthopus_config.encoder_filter_error_gain       = 1;
-    orthopus_config.rate_hz                         = 1000;
-    orthopus_config.perf_compensateexectime         = TRUE;
-=======
     orthopus_config_reset(&orthopus_config);
->>>>>>> 8a7a545b... [Cfg] Fix incorrect addresses in config load/save
   }
 
   // Init AMS sensor
