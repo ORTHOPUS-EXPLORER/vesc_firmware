e9f81fcbac3888807f31b331500a8eeaa213e21f
Thomas Solatges <thomas.solatges@orthopus.com>
2023-07-19 13:24:08 +0200

    renaming variables

--- applications/orthopus/app_orthopus.h
+++ applications/orthopus/app_orthopus.h
@@ -61,7 +61,9 @@ typedef struct
   float speed_now; //actual speed from mc_interface
   float enc_pos; //raw encoder position
   float time_diff;
-  float time_diff_filt;
+  float time_diff_filt;/* -------------------------------------------------------------------------- */
+/*                                  MAIN LOOP                                 */
+/* -------------------------------------------------------------------------- */
   int time_lag_filt;
   int time_lag_compensation;
   bool perf_plot;                             //enables realtime performance plot 
